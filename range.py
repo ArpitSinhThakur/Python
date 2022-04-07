2a5439bb6361956735d6a1a6c8f36553191360d6
ArpitSinhThakur <103022129+ArpitSinhThakur@users.noreply.github.com>
2022-04-07 09:54:22 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,47 @@
+#write a python program to print numbers given in range
+
+for i in range(0,20,2):
+    print(i)
+
+    
+#write a python program to print boolian method
+    c=15
+    print(type(c))
+    print(id(c))
+
+#write a python program to find type of given data
+    a=39
+    b=3.16
+    c='INDIA'
+    print(type(a))
+    print(id(a))
+    print(type(b))
+    print(id(b))
+    print(type(c))
+    print(id(c))
+
+#write a python program to convert integer data to binary data
+    a=69
+    b=bin(a)
+    print(b)
+
+#write a python progrm to convert binary data to integer data
+    a=0b1000101
+    b=int(a)
+    print(b)
+
+#write a python program to convert integer data to octal data
+    a=20
+    b=oct(a)
+    print(b)
+
+#write a python program to convert integer data to hexadecimal
+    a=15
+    b=hex(a)
+    print(b)
+
+#write a python program to convert hexadecimal data to intiger data
+    a=0xf
+    b=int(a)
+    print(b)
+    
