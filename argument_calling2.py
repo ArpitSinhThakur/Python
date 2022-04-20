c2963e1f06ee041270279d40cfbdf664dd5e9a96
ArpitSinhThakur <103022129+ArpitSinhThakur@users.noreply.github.com>
2022-04-20 11:58:10 +0530

    Add files via upload

---
+++ argument_calling2.py
@@ -0,0 +1,8 @@
+#write a python function to understand the keyword argument of a function
+def grocery(item,price):
+    "to display the given argument"
+    print('ITME=%s'%item)
+    print('price=%2.f'%price)
+
+grocery(item='sugar',price=50.75)
+
